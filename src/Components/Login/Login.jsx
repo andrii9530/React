c5bb6ab79539ed2e57d5778366a4db7c33d5d4e2
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2024-08-12 11:33:59 +0300

    Update Login.jsx
    Додав formik

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
- 
+  // Створення схеми валідації для логіну та паролю
+  const LoginSchema = Yup.object().shape({
+    login: Yup.string().required('Обов’язкове поле'),
+    password: Yup.string().required('Обов’язкове поле'),
+  });
+
   return (
-    <div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <input 
-        type="text"
-        placeholder="Login" 
-      />
-      <br></br>
-      <input 
-        type="text"
-        placeholder="Password" 
-      />
-      <br></br>
-      <button>Готово</button>
-    </div>
+    <Formik
+      initialValues={{ login: '', password: '' }}
+      validationSchema={LoginSchema}
+      onSubmit={values => {
+        // Дії після успішного заповнення форми
+        console.log('Логін:', values.login);
+        console.log('Пароль:', values.password);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <div>
+            <Field 
+              type="text" 
+              name="login" 
+              placeholder="Login" 
+            />
+            <ErrorMessage name="login" component="div" />
+          </div>
+          <br />
+          <div>
+            <Field 
+              type="password" 
+              name="password" 
+              placeholder="Password" 
+            />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <br />
+          <button type="submit" disabled={isSubmitting}>
+            Готово
+          </button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
